cf6e78bdf117d253523f2e4848f99adbe6caab30
parksangseok <parkss0157@naver.com>
2024-04-14 20:44:11 +0900

    React JS #8 props

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,20 +1,23 @@
 import { useState } from "react";
+import UserName from "./UserName";
 
-export default function Hello() {
+export default function Hello({ age }) {
 
-    // let name = "Mike"
     const [name, setName] = useState("Mike");
-
-    function changeName() {
-        setName(name === "Mike" ? "Jane" : "Mike");
-    }
+    const msg = age > 19 ? "성인입니다." : "미성년자입니다.";
 
     return (
         <div>
-            <h2>
+            <h2 id="name">
                 {name}
+                ({age}) : {msg}
             </h2>
-            <button onClick={changeName}>Change</button>
-        </div>
+            <UserName name={name}/>
+            <button
+                onClick={() => {
+                    setName(name === "Mike" ? "Jane" : "Mike")
+                }}
+            >Change</button>
+        </div >
     )
 }
