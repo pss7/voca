7c5b5d7e2236fb808556b068a314b9e06f3b8d63
parksangseok <parkss0157@naver.com>
2024-04-14 17:52:11 +0900

    React JS #5 CSS 작성법(module css)

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,11 +1,25 @@
 import World from "./World";
+import "./Hello.css";
+import styles from "./Hello.module.css"
 
 export default function Hello() {
     return (
         <>
-            <h1>Hello</h1>
-            <World />
-            <World />
+            <h1 style={{
+                color: '#f00',
+                borderRight: '2px solid #000',
+                marginBottom: '30px',
+                opacity: 1,
+            }}
+            >
+                Hello
+            </h1>
+            <div className="box">
+                Hello
+            </div>
+            <div className={styles.box}>
+                Hello
+            </div>
         </>
     )
 }
